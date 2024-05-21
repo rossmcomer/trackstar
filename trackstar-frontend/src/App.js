8ea59f757b4251362febc57b30a79c157a85cab1
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-05-21 00:47:38 -0500

    modify .env BACKEND_URL

--- trackstar-frontend/src/App.js
+++ trackstar-frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import React from 'react'
-require('dotenv').config()
 import { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useInitialization } from './hooks'
