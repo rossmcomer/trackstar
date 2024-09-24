edee1353d5c1e8c64686c7c56e068e1778f1f3c8
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-09-24 12:21:22 -0500

    refactor fetchModalData into its own util file

--- trackstar-frontend/src/components/Favorites.js
+++ trackstar-frontend/src/components/Favorites.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import '../App.css'
 import Modal from 'react-modal'
-import ApexCharts from 'apexcharts'
 import { useDispatch, useSelector } from 'react-redux'
 import { createFavorite, removeFavorite } from '../reducers/favorites'
 import { notify } from '../reducers/notification'
+import MarketsTable from './MarketsTable'
+import { fetchModalData } from '../util/fetchModalData'
 
 const Favorites = () => {
   const dispatch = useDispatch()
@@ -61,90 +62,7 @@ const Favorites = () => {
   useEffect(() => {
     if (!modalIsOpen || !selectedCrypto) return
 
-    const fetchModalData = async () => {
-      try {
-        const result = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/${selectedCrypto.id}/market_chart?vs_currency=usd&days=90`,
-        )
-
-        const prices = result.data.prices
-
-        let options = {
-          series: [
-            {
-              name: `${selectedCrypto.symbol.toUpperCase()}/USD`,
-              data: prices.map((price) => [
-                new Date(price[0]).getTime(),
-                price[1],
-              ]),
-            },
-          ],
-          chart: {
-            type: 'area',
-            stacked: false,
-            height: 350,
-            zoom: {
-              type: 'x',
-              enabled: true,
-              autoScaleYaxis: true,
-            },
-            toolbar: {
-              autoSelected: 'zoom',
-            },
-          },
-          title: {
-            text: `${selectedCrypto.name}`,
-            align: 'left',
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          markers: {
-            size: 0,
-          },
-          fill: {
-            type: 'gradient',
-            gradient: {
-              shadeIntensity: 1,
-              inverseColors: false,
-              opacityFrom: 0.5,
-              opacityTo: 0,
-              stops: [0, 90, 100],
-            },
-          },
-          yaxis: {
-            labels: {
-              formatter: function (val) {
-                return val.toFixed(2)
-              },
-            },
-            title: {
-              text: 'Price/USD',
-            },
-          },
-          xaxis: {
-            type: 'datetime',
-          },
-          tooltip: {
-            shared: true,
-            y: {
-              formatter: function (val) {
-                return val.toFixed(4)
-              },
-            },
-          },
-        }
-        const chart = new ApexCharts(
-          document.querySelector('#cryptoChart'),
-          options,
-        )
-        chart.render()
-      } catch (error) {
-        console.error('Error fetching historical data:', error)
-      }
-    }
-
-    fetchModalData()
+    fetchModalData(selectedCrypto)
   }, [modalIsOpen, selectedCrypto])
 
   const addToFavorites = (id) => {
@@ -175,50 +93,14 @@ const Favorites = () => {
     <div className="MarketsContainer" style={{ marginTop: user === null ? '150px' : '20px' }}>
       {user ? (
         <div className="TableContainer">
-          <table className="marketsTable">
-            <thead>
-              <tr id="marketsHeader">
-                <th colSpan="2">Ticker</th>
-                <th>Price</th>
-                <th>24hr Change%</th>
-                <th>Favorite</th>
-                <th>View Chart</th>
-                <th>Market Cap</th>
-                <th>Volume</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cryptos.slice(0, visible).map((crypto) => (
-                <tr key={crypto.id} className="cryptoRow">
-                  <td>
-                    <img src={crypto.image} alt="Logo" width="20px"></img>
-                  </td>
-                  <td>{crypto.symbol.toUpperCase()}</td>
-                  <td>${crypto.current_price.toLocaleString()}</td>
-                  <td>{crypto.price_change_percentage_24h.toFixed(2)}%</td>
-                  <td>
-                    <button
-                      id="favBtn"
-                      onClick={() => addToFavorites(crypto.id)}
-                    >
-                      {favorites
-                        .map((fav) => fav.coingeckoId)
-                        .includes(crypto.id)
-                        ? '★'
-                        : '☆'}
-                    </button>
-                  </td>
-                  <td>
-                    <button onClick={() => openModal(crypto)} id="viewChartBtn">
-                      View
-                    </button>
-                  </td>
-                  <td>${crypto.market_cap.toLocaleString()}</td>
-                  <td>${crypto.total_volume.toLocaleString()}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MarketsTable
+            cryptos={cryptos}
+            visible={visible}
+            addToFavorites={addToFavorites}
+            favorites={favorites}
+            openModal={openModal}
+            loadMore={loadMore}
+          />
           {visible < cryptos.length && (
             <button onClick={loadMore} className="load-more-button">
               Load More
--- trackstar-frontend/src/components/Markets.js
+++ trackstar-frontend/src/components/Markets.js
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { createFavorite, removeFavorite } from '../reducers/favorites'
 import { notify } from '../reducers/notification'
 import MarketsTable from './MarketsTable'
+import { fetchModalData } from '../util/fetchModalData'
 
 const Markets = () => {
   const dispatch = useDispatch()
@@ -58,103 +59,7 @@ const Markets = () => {
   useEffect(() => {
     if (!modalIsOpen || !selectedCrypto) return
 
-    const fetchModalData = async () => {
-      try {
-        const result = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/${selectedCrypto.id}/market_chart?vs_currency=usd&days=90`,
-        )
-
-        const prices = result.data.prices
-
-        let options = {
-          series: [
-            {
-              name: `${selectedCrypto.symbol.toUpperCase()}/USD`,
-              data: prices.map((price) => [
-                new Date(price[0]).getTime(),
-                price[1],
-              ]),
-            },
-          ],
-          grid: {
-            borderColor: '#000000',
-            xaxis: {
-              lines: {
-                show: true,
-                color: '#1c19ec',
-              }
-            }
-          },
-          stroke: {
-            colors: ['#1c19ec']
-          },
-          chart: {
-            type: 'area',
-            stacked: false,
-            height: 350,
-            zoom: {
-              type: 'x',
-              enabled: true,
-              autoScaleYaxis: true,
-            },
-            toolbar: {
-              autoSelected: 'zoom',
-            },
-          },
-          title: {
-            text: `${selectedCrypto.name}`,
-            align: 'left',
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          markers: {
-            size: 0,
-          },
-          fill: {
-            type: 'gradient',
-            gradient: {
-              shadeIntensity: 1,
-              inverseColors: false,
-              opacityFrom: 0.5,
-              opacityTo: 0,
-              stops: [0, 90, 100],
-              gradientToColors: ['#1c19ec']
-            },
-          },
-          yaxis: {
-            labels: {
-              formatter: function (val) {
-                return val.toFixed(2)
-              },
-            },
-            title: {
-              text: 'Price/USD',
-            },
-          },
-          xaxis: {
-            type: 'datetime',
-          },
-          tooltip: {
-            shared: true,
-            y: {
-              formatter: function (val) {
-                return val.toFixed(4)
-              },
-            },
-          },
-        }
-        const chart = new ApexCharts(
-          document.querySelector('#cryptoChart'),
-          options,
-        )
-        chart.render()
-      } catch (error) {
-        console.error('Error fetching historical data:', error)
-      }
-    }
-
-    fetchModalData()
+    fetchModalData(selectedCrypto)
   }, [modalIsOpen, selectedCrypto])
 
   const addToFavorites = (id) => {
